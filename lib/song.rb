fabb474cdb1e85ae2dcc63b49c16f14b8d3c863e
Nathan  Visak <nathan.visak@gmail.com>
2018-03-26 17:35:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -77,6 +77,7 @@ class Song
       artist = Artist.find_or_create_by_name(parts[0])
       genre = Genre.find_or_create_by_name(parts[2].delete ".mp3")
       song = Song.new(song_name, artist, genre)
+      song.name
     end
 
 end
